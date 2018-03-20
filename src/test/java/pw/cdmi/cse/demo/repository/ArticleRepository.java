6ed495b692c35ec0018a85c4e707b59211817dd3
974515535 <974515535@qq.com>
2018-03-20 17:33:59 +0800

    去多余的东西

--- src/test/java/pw/cdmi/cse/demo/repository/ArticleRepository.java
+++
@@ -1,9 +0,0 @@
-package pw.cdmi.cse.demo.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-import pw.cdmi.cse.demo.model.Article;
-
-public interface ArticleRepository extends MongoRepository<Article, Long> {
-
-
-}
--- src/test/java/pw/cdmi/cse/demo/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package pw.cdmi.cse.demo.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import pw.cdmi.cse.demo.model.User;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-
-}
